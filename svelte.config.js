c38b2812b07436a0d6d8041fef7d6234b4a5e622
Paul Brenner <paulwbrenner@gmail.com>
2023-06-06 21:30:41 +0900

    adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-node';
 // import imagetools from 'vite-imagetools';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
